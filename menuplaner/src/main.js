357a37280936251bcd70c1b4a20476cfadc081c6
Cyril Gabriele <GabrieleC@regatron.com>
2024-08-07 14:25:11 +0200

    test commit from other workstation

--- menuplaner/src/main.js
+++ menuplaner/src/main.js
@@ -4,5 +4,3 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
-
-// https://huggingface.co/MaziyarPanahi/calme-2.1-qwen2-72b => use this model for API calls

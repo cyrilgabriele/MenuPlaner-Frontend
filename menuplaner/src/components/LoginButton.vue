b44664d465ae00e13fd315f05ecf97ba5ff99cda
Cyril Gabriele <GabrieleC@regatron.com>
2024-08-13 09:33:45 +0200

    fixed little stuff as formatation in login button and added logout button

--- menuplaner/src/components/LoginButton.vue
+++ menuplaner/src/components/LoginButton.vue
@@ -1,15 +1,15 @@
-<!-- components/LoginButton.vue -->
+  
+<script setup>
+import { useAuth0 } from '@auth0/auth0-vue';
+
+const { loginWithRedirect } = useAuth0();
+
+const login = () => {
+loginWithRedirect();
+};
+</script>
+
 <template>
     <button @click="login">Log In</button>
-  </template>
-  
-  <script setup>
-  import { useAuth0 } from '@auth0/auth0-vue';
-  
-  const { loginWithRedirect } = useAuth0();
-  
-  const login = () => {
-    loginWithRedirect();
-  };
-  </script>
+</template>
   

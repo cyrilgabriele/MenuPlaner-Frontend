a11fb7cc42bef412d0a6d00c4439cf0a2ec14c1c
Cyril Gabriele <GabrieleC@regatron.com>
2024-08-13 11:35:08 +0200

    added a logout button

--- menuplaner/src/views/Home.vue
+++ menuplaner/src/views/Home.vue
@@ -1,12 +1,14 @@
 <script setup>
   import MenuForm from "../components/MenuForm.vue"
-  import LoginButton from "@/components/LoginButton.vue";
+  import LoginButton from "@/components/LoginButton.vue"
+  import LogoutButton from "@/components/LogoutButton.vue";
   console.log("Hello form Home")
 </script>
 
 <template>
     <div>
       <LoginButton />
+      <LogoutButton />
       <h1>Home Page</h1>
       <h2>Welcome to the Home Page!</h2>
       <p>Enter your Menuplan ideas:</p>

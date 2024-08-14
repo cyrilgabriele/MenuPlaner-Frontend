8394efb169ae4ac61f5b7c76c0612b2a525c366d
Cyril Gabriele <GabrieleC@regatron.com>
2024-08-14 09:24:29 +0200

    changed the sequence

--- menuplaner/src/components/LogoutButton.vue
+++ menuplaner/src/components/LogoutButton.vue
@@ -1,15 +1,15 @@
-<template>
-    <div>
-      <button @click="handleLogout">Log out</button>
-    </div>
-  </template>
-  
-  <script setup>
+<script setup>
   import { useAuth0 } from '@auth0/auth0-vue';
   
   const { logout } = useAuth0();
   const handleLogout = () => {
     logout({ logoutParams: { returnTo: window.location.origin } });
   };
-  </script>
+</script>
+
+<template>
+    <div>
+      <button @click="handleLogout">Log out</button>
+    </div>
+</template>
   

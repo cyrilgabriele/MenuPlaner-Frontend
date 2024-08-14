d93a1996102e1d8ef975c6c2b07b9749db3c5915
Cyril Gabriele <GabrieleC@regatron.com>
2024-08-14 16:27:56 +0200

    adjusted so that tailwind works fine

--- menuplaner/src/components/Navbar.vue
+++ menuplaner/src/components/Navbar.vue
@@ -5,23 +5,21 @@ import LogoutButton from './LogoutButton.vue';
 import SignupButton from './SignupButton.vue';
 
 const { isAuthenticated } = useAuth0();
-console.log("Hello from NavBar.vue")
 </script>
 
 <template>
-    <nav>
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/meal-plans">Weekly Menu</router-link></li>
-        <li><router-link to="/profile">Profile</router-link></li>
-        <template v-if="!isAuthenticated">
-          <SignupButton />
-          <LoginButton />
-        </template>
-        <template v-if="isAuthenticated">
-          <LogoutButton />
-        </template>
-      </ul>
-    </nav>
+  <nav class="bg-gray-800 p-4">
+    <ul class="flex space-x-4">
+      <li><router-link to="/" class="text-white hover:text-gray-300">Home</router-link></li>
+      <li><router-link to="/meal-plans" class="text-white hover:text-gray-300">Weekly Menu</router-link></li>
+      <li><router-link to="/profile" class="text-white hover:text-gray-300">Profile</router-link></li>
+      <template v-if="!isAuthenticated">
+        <li><SignupButton class="text-white hover:text-gray-300" /></li>
+        <li><LoginButton class="text-white hover:text-gray-300" /></li>
+      </template>
+      <template v-if="isAuthenticated">
+        <li><LogoutButton class="text-white hover:text-gray-300" /></li>
+      </template>
+    </ul>
+  </nav>
 </template>
-  
--- menuplaner/tailwind.config.js
+++ menuplaner/tailwind.config.js
@@ -1,31 +1,13 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, 
   theme: {
-    colors: {
-      gray: colors.gray,
-      blue: colors.sky,
-      red: colors.rose,
-      pink: colors.fuchsia,
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    }
+    extend: {},
   },
   variants: {
-    extend: {
-      borderColor: ['focus-visible'],
-      opacity: ['disabled'],
-    }
-  }
+    extend: {},
+  },
+  plugins: [],
 }

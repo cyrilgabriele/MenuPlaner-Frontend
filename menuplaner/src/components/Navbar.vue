7cf004aef2133dbbf5fd6ee0289fb1cd4b67469f
Cyril Gabriele <GabrieleC@regatron.com>
2024-08-14 15:48:41 +0200

    introduced a singup button and refactored the navbar to add these buttons

--- menuplaner/src/components/Navbar.vue
+++ menuplaner/src/components/Navbar.vue
@@ -1,4 +1,10 @@
 <script setup>
+import { useAuth0 } from '@auth0/auth0-vue';
+import LoginButton from './LoginButton.vue';
+import LogoutButton from './LogoutButton.vue';
+import SignupButton from './SignupButton.vue';
+
+const { isAuthenticated } = useAuth0();
 console.log("Hello from NavBar.vue")
 </script>
 
@@ -8,11 +14,14 @@ console.log("Hello from NavBar.vue")
         <li><router-link to="/">Home</router-link></li>
         <li><router-link to="/meal-plans">Weekly Menu</router-link></li>
         <li><router-link to="/profile">Profile</router-link></li>
+        <template v-if="!isAuthenticated">
+          <SignupButton />
+          <LoginButton />
+        </template>
+        <template v-if="isAuthenticated">
+          <LogoutButton />
+        </template>
       </ul>
     </nav>
 </template>
-  
-  <style scoped>
-  /* Add styles for your navigation bar here */
-  </style>
   

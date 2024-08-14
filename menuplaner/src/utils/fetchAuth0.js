b193bce02216ad9d33363966540f17290868a37a
Cyril Gabriele <GabrieleC@regatron.com>
2024-08-14 15:55:27 +0200

    adjusted the isAuthenticated.value access in the fetch

--- menuplaner/src/utils/fetchAuth0.js
+++ menuplaner/src/utils/fetchAuth0.js
@@ -3,15 +3,15 @@ import { useAuth0 } from "@auth0/auth0-vue"
 async function fetchAuth0 (isAuthenticated) {
     const { user } = useAuth0()
     console.log("in fetchAuth0(): isAuthenticated = ", isAuthenticated)
-    console.log("is authenticated = ", isAuthenticated._value)
-    isAuthenticated = isAuthenticated._value
+    console.log("is authenticated = ", isAuthenticated.value)
+    isAuthenticated = isAuthenticated.value
     if (isAuthenticated === false) {
         console.error("NOT AUTHENTICATED!")
     } else {
         console.log("Is authenticated")
         const userID = user.value.sub
         console.log("userID = ", userID)
-        console.log("In fetchAuth0: userID = ", userID.split('|')[1])
+        //console.log("In fetchAuth0: userID = ", userID.split('|')[1])
         return userID.split('|')[1]
     }
 }

bcefc3ec6039b4827f3220bf322dc702df6c7007
Cyril Gabriele <cyril.gabriele@gmail.com>
2024-08-21 14:54:50 +0200

    implemented the pop-up view for a specific meal/recipe

--- menuplaner/src/components/MenuTable.vue
+++ menuplaner/src/components/MenuTable.vue
@@ -1,5 +1,7 @@
 <script setup>
-import AcceptButton from './AcceptButton.vue';
+import { ref } from 'vue'
+import AcceptButton from './AcceptButton.vue'
+import Modal from './Modal.vue'
 
 const props = defineProps({
   menu: {
@@ -7,15 +9,27 @@ const props = defineProps({
   },
 })
 
-const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-const meals = ['Breakfast', 'Lunch', 'Dinner'];
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+const meals = ['Breakfast', 'Lunch', 'Dinner']
+
+const showModal = ref(false)
+const selectedMeal = ref({ title: '', description: '' })
+
+function openModal(day, meal) {
+  selectedMeal.value = props.menu[day][meal]
+  showModal.value = true
+}
+
+function closeModal() {
+  showModal.value = false; // Hide the modal
+}
 </script>
 
 <template>
   <div class="overflow-x-auto">
     <table class="w-full border-collapse">
       <thead>
-        <tr class="text-lg">
+        <tr class="text-sm">
           <th class="p-2"></th>
           <th class="p-2" v-for="day in days" :key="day">{{ day }}</th>
         </tr>
@@ -23,18 +37,25 @@ const meals = ['Breakfast', 'Lunch', 'Dinner'];
       <tbody>
         <tr v-for="meal in meals" :key="meal" class="border-t text-sm">
           <td class="p-2 font-semibold">{{ meal }}</td>
-          <td v-for="day in days" :key="day" class="p-2 text-sm">
+          <td v-for="day in days" :key="day" class="p-2">
             <input 
               class="w-full p-2 text-gray-800 border border-teal-500 rounded-md text-xs"
               v-model="props.menu[day][meal].title" 
               placeholder="Add meal name" 
+              @click="openModal(day, meal)"
             />
           </td>
         </tr>
       </tbody>
     </table>
-    <div>
-      <AcceptButton :menuPlan='props.menu' class="w-full bg-teal-500 p-2 rounded-md hover:bg-teal-600 focus:outline-none" />
-    </div>
   </div>
+  <div>
+    <AcceptButton :menuPlan='props.menu' class="w-full bg-teal-500 p-2 rounded-md hover:bg-teal-600 focus:outline-none" />
+  </div>
+  <Modal 
+    :visible="showModal" 
+    :title="selectedMeal.title" 
+    :description="selectedMeal.description" 
+    @close="closeModal" 
+  />
 </template>
---
+++ menuplaner/src/components/Modal.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+visible: {
+    type: Boolean,
+    required: true
+},
+title: {
+    type: String,
+    required: true
+},
+description: {
+    type: String,
+    required: true
+}
+})
+
+const emit = defineEmits(['close'])
+
+function closeModal() {
+emit('close')
+}
+</script>
+
+<template>
+    <div v-if="visible" class="fixed inset-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50">
+      <div class="bg-white p-4 rounded-md w-96">
+        <h2 class="text-xl font-semibold mb-2 text-gray-800">{{ title }}</h2>
+        <p class="text-gray-800">{{ description }}</p>
+        <button class="mt-4 bg-teal-500 text-white px-2 py-1 rounded-md" @click="closeModal">Close</button>
+      </div>
+    </div>
+</template>
+  

76ff214888e109f488d1133d5065fbdcfea12742
Cyril Gabriele <cyril.gabriele@gmail.com>
2024-09-02 11:33:40 +0200

    found nasty mistake => .value results in undefined if i pass a ref variable as a prop and then pass this prop varibale to a function.

--- menuplaner/src/utils/apiService.js
+++ menuplaner/src/utils/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export function generateMenuplan(menuIdeaUser, LLMResponse) {
-  // console.log("generateMenuplan: menuIdeaUser: ", menuIdeaUser.value)
+  console.log("generateMenuplan: menuIdeaUser: ", menuIdeaUser.value)
   axios.post('http://localhost:3000/menuplan/generateMenuplan', { 
     custom_prompt: menuIdeaUser.value
   })
@@ -22,13 +22,25 @@ export function handleAcceptMenuplan(auth0_user_id, custom_prompt, menuPlan) {
   console.log("handleAcceptMenu: menuPlan: ", menuPlan)
   axios.post('http://localhost:3000/menuplan/saveMenuplan', {
     auth0_user_id: auth0_user_id,
-    custom_prompt: custom_prompt.value,
+    custom_prompt: custom_prompt, // here no .value as it is a prop now not
     meals: menuPlan,
   })
   .then(response => {
       console.log('Response AcceptButton: \n', response.data)
   })
   .catch(error => {
-      console.log("Error during fetch: ", error)
+      console.log("in handleAcceptMenuplan: Error during fetch: ", error)
   })
+}
+
+export function getMenuplan(auth0_user_id) {
+  axios.post('http://localhost:3000/menuplan/getMenuplan', {
+    auth0_user_id: auth0_user_id
+  })
+  .then(response => {
+    console.log('Response getMenuplan: \n', response.data)
+  })
+  .catch(error => {
+    console.log("in getMenuplan: Error during fetch: ", error)
+})
 }
